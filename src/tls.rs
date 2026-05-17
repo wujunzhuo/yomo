eb2c0ebc2ade2b8fc5f7d50a282388e6a2e249d8
wujunzhuo <wujunzhuo@hotmail.com>
2026-05-17 18:24:59 +0800

    fix rustls init proiver

--- src/tls.rs
+++ src/tls.rs
@@ -105,6 +105,12 @@ impl TlsProvider {
         key_pem: Option<Vec<u8>>,
         mutual: bool,
     ) -> Result<Self, RustlsError> {
+        rustls_crate::crypto::aws_lc_rs::default_provider()
+            .install_default()
+            .map_err(|e| {
+                RustlsError::General(format!("failed to install default provider: {:?}", e))
+            })?;
+
         Ok(Self {
             ca_cert: if let Some(buf) = ca_cert_pem {
                 Some(into_certificate(&buf)?)
